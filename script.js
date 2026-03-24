bbf8b88a849cca6a2e7fa953352ea38693681c07
hend5rashad <hend5rashad@gmail.com>
2026-03-24 11:33:52 +0000

    History for your questions, css is still in progress

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const mahtInput = document.getElementById("mahtInput");
 const solveButton = document.getElementById("solveButton");
 const resultBox = document.getElementById("resultBox");
+const historyList = document.getElementById('historyList');
+
+let myHistory = [];
+
 
 solveButton.onclick = function () {
     const inputValue = mahtInput.value;
@@ -15,6 +19,8 @@ solveButton.onclick = function () {
         resultBox.innerHTML = "The answer is: " + theAnswer;
         
         resultBox.style.color = "green";
+
+        addToHistory(inputValue + " = " + theAnswer)
     } catch (error) {
         resultBox.innerHTML = "Sorry, I can't solve this!";
         resultBox.style.color = "red";
@@ -26,3 +32,22 @@ mahtInput.onkeydown = function(e) {
         solveButton.onclick();
     }
 };
+
+function addToHistory(text) {
+    myHistory.unshift(text);
+
+    if (myHistory.length > 5) {
+        myHistory.pop();
+    }
+
+    showHistory();
+}
+
+function showHistory() {
+    let html = "";
+    for (let i = 0; i < myHistory.length; i++) {
+        html += "<li>" + myHistory[i] + "</li>";
+    }
+    
+    historyList.innerHTML = html
+}
